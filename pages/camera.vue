e288ba602b6e3a6533a646fcf5b99b9d9beb8c8e
Wattana-30 <wattana.po@ku.th>
2022-07-29 23:11:37 +0700

    Auto stash before rebase of "origin/master"
    [Sing] camera farm

---
+++ pages/camera.vue
@@ -0,0 +1,30 @@
+<template>
+
+<div class="container text-center">
+		<div class="card shadow">
+			<div class="card-body">
+
+				<!-- Header -->
+				<div class="row align-items-start">
+					<div class="col-2">
+						<Logo />
+					</div>
+					<div class="col">
+						<h2>Camera farm</h2>
+					</div>
+				</div>
+
+				<!-- Content -->
+				<div class="row align-items-start">
+					<div class="col">
+						<Showpicture />
+					</div>
+					<div class="col">
+						<h2>{{ dateTime }}</h2>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</template>
